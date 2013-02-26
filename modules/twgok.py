8c21b7f12a3275d67b285ec08cb9063e66c3019a
Jónatan Nilsson <jonatan@nilsson.is>
2013-02-26 10:05:40 +0000

    Implemented base support for the TWGOK module.

--- modules/twgok.py
+++ modules/twgok.py
@@ -1,14 +1,20 @@
 from plugins import IrcModule
 
 class TWGOK(IrcModule):
-	irc = None
+    irc = None
 
-	def __init__(self, irc):
-		self.irc = irc
+    def __init__(self, irc):
+        self.irc = irc
 
-	def supports(self, channel, user, message):
-		if (message.startswith("!twgok")):
-			return True
+    def supports(self, channel, user, message):
+        if (message.startswith("!twgok")):
+            return True
 
-	def on_message(self, channel, user, message):
-		self.irc.send_message(channel, "%s: Please try again later." % user)
+    def on_message(self, channel, user, message):
+        self.irc.send_message(channel, "%s: Please try again later." % user)
+
+    def name(self):
+        return "TWGOK"
+
+    def version(self):
+        return "0.0.2"
